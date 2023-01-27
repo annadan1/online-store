b7e139fdc5a2d17b89fac614eecd52b9b729f675
annadan1 <anna_s92_@mail.ru>
2023-01-27 15:42:04 +0700

    Added buttons

---
+++ src/components/Content/Buttons/Buttons.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import {
+  buttonsGroup, cartCalc, buttonCalc, buttonAddToCart,
+} from './ButtonsGroup.module.scss';
+import { actions } from '../../../slices/cartSlices';
+import Add from '../../../assets/images/add.svg';
+import Minus from '../../../assets/images/minus.svg';
+
+const Buttons = ({ item, addedToCart }) => {
+  const dispatch = useDispatch();
+
+  const handleAddClick = (i) => {
+    dispatch(actions.addItem(i));
+  };
+
+  const handleMinusClick = (i) => {
+    dispatch(actions.minusItem(i));
+  };
+
+  return (
+    <div className={buttonsGroup}>
+      {addedToCart ? (
+        <div className={cartCalc}>
+          <button type="button" className={buttonCalc} aria-label="Minus" onClick={() => handleMinusClick(item)}><Minus /></button>
+          <span>{ `${addedToCart.count}`}</span>
+          <button type="button" className={buttonCalc} aria-label="Add" onClick={() => handleAddClick(item)}><Add /></button>
+        </div>
+      ) : (
+        <button type="button" className={buttonAddToCart} onClick={() => handleAddClick(item)}>
+          <span>Добавить в корзину</span>
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default Buttons;
