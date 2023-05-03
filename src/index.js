cc0f617d55fc3755a421fd00ee2df5c613af5940
annadan1 <anna_s92_@mail.ru>
2023-05-03 18:25:33 +0400

    Change router

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './components/App/App';
 import store from './slices/index.js';
 import SearchParamsProvider from './provider/SearchParamsProvider';
@@ -12,11 +12,11 @@ const init = () => {
   root.render(
     <React.StrictMode>
       <Provider store={store}>
-        <BrowserRouter>
+        <HashRouter>
           <SearchParamsProvider>
             <App />
           </SearchParamsProvider>
-        </BrowserRouter>
+        </HashRouter>
       </Provider>
     </React.StrictMode>,
   );
